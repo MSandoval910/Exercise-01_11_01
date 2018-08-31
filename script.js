84d8b09b2f791ee38c9c51934d72bfbc3bd19f3e
Unknown <MSandoval910@west-mec.org>
2018-08-30 18:26:45 -0700

    third commit
    added some JSON in the code

--- script.js
+++ script.js
@@ -10,9 +10,9 @@
 "use strict";
 // global variables
 var selectedCity = "Tucson, AZ"; // default locations
-var weatherReport = null;
+var weatherReport = null; // holds our response data
 // var to hold our XHR object 
-var httpRequest = false;
+var httpRequest = false; // have an XHR OBJECT?
 
 // function to get a request object 
 function getRequestObject() {
@@ -25,6 +25,21 @@ function getRequestObject() {
         return false;
     }
    return httpRequest;
+}
+// function is an event handler for onreadystatechange
+// get the weather data if successful 
+function fillWeather(){
+    // check the readyState 4 - done 
+    if(httpRequest.readyState === 4 && httpRequest.status === 200){
+        weatherReport = JSON.parse(httpRequest.responseText);
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday"];
+        var dateValue = new Date(weatherReport.daily.data[0].time);
+        var dayOfWeek = dateValue.getDay();
+        var rows = document.querySelectorAll("section.week table tbody tr");
+        document.querySelector("section.week table caption").innerHTML = selectedCity;
+         document.querySelector("section.week table caption").style.display = "block";
+         document.querySelector("section.week table").style.display = "inline-block";
+    }
 }
 
 // get the weather in response too the click events on the city locations
@@ -59,6 +74,8 @@ function getWeather(evt) {
     // target request 
     httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng=" + longitude, true);
     httpRequest.send(null);
+    // event listener for onreadystate change 
+    httpRequest.onreadystatechange = fillWeather;
 }
 //retrieve li elements holding city location choices
 var locations = document.querySelectorAll("section ul li");
