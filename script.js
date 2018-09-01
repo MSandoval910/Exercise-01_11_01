ce248d21eb87978b17fff30451e71bf0dcc3bf28
Unknown <MSandoval910@west-mec.org>
2018-08-31 17:34:22 -0700

    final commit
    added final credit to the website darksky also added the percentages and then also made the sun images different colors

--- script.js
+++ script.js
@@ -24,21 +24,73 @@ function getRequestObject() {
         document.querySelector("p.error").style.display = "block";
         return false;
     }
-   return httpRequest;
+    return httpRequest;
 }
 // function is an event handler for onreadystatechange
 // get the weather data if successful 
-function fillWeather(){
+function fillWeather() {
     // check the readyState 4 - done 
-    if(httpRequest.readyState === 4 && httpRequest.status === 200){
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
         weatherReport = JSON.parse(httpRequest.responseText);
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday"];
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         var dateValue = new Date(weatherReport.daily.data[0].time);
         var dayOfWeek = dateValue.getDay();
         var rows = document.querySelectorAll("section.week table tbody tr");
         document.querySelector("section.week table caption").innerHTML = selectedCity;
-         document.querySelector("section.week table caption").style.display = "block";
-         document.querySelector("section.week table").style.display = "inline-block";
+        // retrieve the row cells
+        for (var i = 0; i < rows.length; i++) {
+            var firstCell = rows[i].getElementsByTagName("td")[0];
+            var secondCell = rows[i].getElementsByTagName("td")[1];
+            var thirdCell = rows[i].getElementsByTagName("td")[2];
+            // set the weekday for the row
+            firstCell.innerHTML = days[dayOfWeek];
+            if (dayOfWeek + 1 === 7) {
+                dayOfWeek = 0;
+            }
+            else {
+                dayOfWeek++;
+            }
+            // get the percentage of sunlight
+            var sun = Math.round((1 - weatherReport.daily.data[i].cloudCover) * 100, 0);
+            // change the color of the sun symbol based on %
+            if (sun > 90){
+                secondCell.style.color = "rgb(255, 171, 0)";
+            }
+            else if(sun > 80) {
+                secondCell.style.color = "rgb(255, 179, 25)";
+            }
+             else if(sun > 70) {
+                secondCell.style.color = "rgb(255, 188, 51)";
+            }
+             else if(sun > 60) {
+                secondCell.style.color = "rgb(255, 196, 77)"; 
+            }
+             else if(sun > 50) {
+                secondCell.style.color = "rgb(255, 205, 102)";
+            }
+             else if(sun > 40) {
+                secondCell.style.color = "rgb(255, 213, 128)";
+            }
+             else if(sun > 30) {
+                secondCell.style.color = "rgb(255, 221, 153)";
+            }
+             else if(sun > 20) {
+                secondCell.style.color = "rgb(255, 230, 179)";
+            }
+             else if(sun > 10) {
+                secondCell.style.color = "rgb(255, 238, 204)";
+            }
+           else {
+               secondCell.style.color = "rgb(255, 247, 230)";
+           }
+            secondCell.style.fontSize = "2.5em";
+            thirdCell.innerHTML = sun + "%";
+        }
+
+        // turn on the table display
+        document.querySelector("section.week table caption").style.display = "block";
+        document.querySelector("section.week table").style.display = "inline-block";
+         document.querySelector("section.week p.credit").style.display = "block";
     }
 }
 
@@ -67,7 +119,7 @@ function getWeather(evt) {
     // test for XHR object 
     if (!httpRequest) {
         httpRequest = getRequestObject();
-        
+
     }
     // protect against open request 
     httpRequest.abort();
